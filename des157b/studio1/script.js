7c590a206a2773ff3df74f9e31583de8e5fc4f9d
kendracleary <kendracsf@gmail.com>
2022-01-23 11:29:36 -0800

    studio 1 done

--- des157b/studio1/script.js
+++ des157b/studio1/script.js
@@ -1,18 +1,29 @@
-(function(){
-    'use strict';
+(function() {
 
     const one = document.querySelector('#one');
-    const full = document.querySelector('.fa-expand');
-    const again = document.querySelector('#again');
+    const controls = document.querySelector('#controls');
+    const playToggle = document.querySelector('.fa-play');
     const paragraph1 = document.querySelector('#paragraph1');
     const paragraph2 = document.querySelector('#paragraph2');
     const paragraph3 = document.querySelector('#paragraph3');
     const intervalID = setInterval(checkTime, 1000);
+    let playing = false;
+
+    playToggle.addEventListener('click', function(){
+        if (!playing){
+            one.play();
+            controls.className = "hidden";
+        } else {
+            one.pause();
+            playToggle.className = 'fa-solid fa-play';
+        }
+        playing = !playing
+    })
 
     const quote = {
-        start: [1, 5, 9],
-        stop: [24, 24, 24],
-        line: [paragraph1, paragraph2, paragraph3]
+    start: [1, 5, 9],
+    stop: [24, 24, 24],
+    line: [paragraph1, paragraph2, paragraph3]
     }
 
     function checkTime(){
@@ -25,12 +36,4 @@
         }
     }
 
-    full.addEventListener('click', function(){
-        if (!document.fullscreenElement){
-            document.documentElement.requestFullscreen();
-        } else {
-            document.exitFullscreen();
-        }
-    })
-
-})();
+    })();
